a952de38b7ba3121aee85b3a44ffb649a5481c52
Carlos Wimmer <coiote777@gmail.com>
2021-05-15 12:46:55 -0300

    Fix unusede dependency

--- src/components/Drawer/styles.ts
+++ src/components/Drawer/styles.ts
@@ -1,5 +1,4 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core'
-import { useCart } from '../../hooks/cart'
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
