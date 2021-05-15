35b1bfd4d2e482f74db5c8b48e8bf0bf98af3b22
Carlos Wimmer <coiote777@gmail.com>
2021-05-15 12:44:13 -0300

    Create Drawer

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Divider,
+  Drawer as DrawerMui,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+import { useCart } from '../../hooks/cart'
+import CartItem from '../CartItem'
+import { useStyles } from './styles'
+
+interface DrawerProps {
+  cartOpen: boolean
+  handleCartOpen: (cartOpen: boolean) => void
+}
+
+const Drawer = ({ cartOpen, handleCartOpen }: DrawerProps) => {
+  const { cartItems, handleAddToCart, handleRemoveFromCart, getTotalPrices } =
+    useCart()
+  const { root, drawerHeader, emptyCart } = useStyles()
+
+  return (
+    <DrawerMui
+      anchor="right"
+      open={cartOpen}
+      onClose={() => handleCartOpen(false)}
+    >
+      <div className={root}>
+        <div className={drawerHeader}>
+          <Box p={2}>
+            <Typography variant="h6" component="h3">
+              <Grid container justify="space-between">
+                <Grid item>
+                  <strong>Your Cart</strong>
+                </Grid>
+                <Grid item>Total $ {getTotalPrices.toFixed(2)}</Grid>
+              </Grid>
+            </Typography>
+          </Box>
+          <Divider />
+        </div>
+        <Box p={2}>
+          <Grid container spacing={3}>
+            {!cartItems.length && (
+              <Grid item className={emptyCart}>
+                <h2>... is empty</h2>
+              </Grid>
+            )}
+
+            {cartItems.map((item) => (
+              <Grid item>
+                <CartItem
+                  key={item.id}
+                  product={item}
+                  handleAddToCart={handleAddToCart}
+                  handleRemoveFromCart={handleRemoveFromCart}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </div>
+    </DrawerMui>
+  )
+}
+
+export default Drawer
---
+++ src/components/Drawer/styles.ts
@@ -0,0 +1,29 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
+import { useCart } from '../../hooks/cart'
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 450,
+      paddingTop: 72,
+    },
+
+    drawerHeader: {
+      position: 'fixed',
+      top: 0,
+      right: 0,
+      width: 450,
+      height: 64,
+      backgroundColor: '#424242',
+      zIndex: 1,
+    },
+
+    emptyCart: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 'calc(100vh - 120px)',
+      width: '100%',
+    },
+  }),
+)
