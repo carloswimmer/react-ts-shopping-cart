4791220d3ffc3b65fb15528dcee73869dd586070
Carlos Wimmer <coiote777@gmail.com>
2021-05-15 12:42:16 -0300

    Create App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Products from './components/Products'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import AppBar from './components/AppBar'
+import { CartProvider } from './hooks/cart'
 
-function App() {
+const client = new QueryClient()
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={client}>
+      <CartProvider>
+        <AppBar />
+        <Products />
+      </CartProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
